46651e86f202a3113250923865f71e713672b8e1
Krueger, Brandon <Brandon.Krueger@wwt.com>
2015-10-15 10:41:15 -0500

    Initial checkin

---
+++ angular-currency.js
@@ -0,0 +1,51 @@
+/*
+ * angular-currenct v0.0.1
+ * (c) 2015-2016 Brandon Krueger <brandon.c.krueger@gmail.com>
+ * License: MIT
+ */
+
+angular.module('brandonckrueger.angular-currency', [])
+.directive('angularCurrency', [function () {
+	'use strict';
+	
+	return {
+		'require': '?ngModel',
+		'restrict': 'A',
+		'compile': compile
+	};
+	
+	function compile(tElem, tAttrs) {
+		var isInputText = tElem.is('input:text');
+
+		return function(scope, elem, attrs, controller) {
+			elem.autoNumeric(scope.$eval(attrs.angularCurrency));
+			var updateElement = function (newVal) {
+				if ($.isNumeric(newVal)) {
+					elem.autoNumeric('set', newVal);
+				}
+			};
+
+			if (controller && isInputText) {
+				scope.$watch(tAttrs.ngModel, function () {
+					controller.$render();
+				});
+
+				controller.$render = function () {
+					updateElement(controller.$viewValue);
+				};
+
+				elem.on('change', function () {
+					scope.$apply(function () {
+						controller.$setViewValue(elem.autoNumeric('get'));
+					});
+				});
+			} else {
+				if (isInputText) {
+					attrs.$observe('value', function (val) {
+						updateElement(val);
+					});
+				}
+			}
+		};
+	}
+}]);
